62c72d4536dfac8338954fd994fed658a0691880
Pedro Neiva Alves <pedro.neiva@gmail.com>
2016-09-01 13:23:41 -0300

    Solution to 1183 problem: Above the Main Diagonal

---
+++ Extremely Basic/1183.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main()
+{
+  int i=0, j=0, c=0;
+  double n=0.0, t=0.0;
+  double m[12][12];
+  char o='a';
+
+  scanf("%c\n", &o);
+
+  for(i=0; i<12; i++)
+  {
+    for(j=0; j<12; j++)
+    {
+      scanf("%lf\n", &n);
+      m[i][j] = n;
+    }
+  }
+
+  for(i=0; i<12; i++)
+  {
+    for(j=0; j<12; j++)
+    {
+      if(i<j)
+      {
+        t+=m[i][j];
+        c++;
+      }
+    }
+  }
+
+  if(o == 'M')
+  {
+    t/=c;
+  }
+
+  printf("%.1lf\n", t);
+
+  return 0;
+}
